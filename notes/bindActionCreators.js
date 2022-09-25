02aebbfb83948b6345c30b73c3061e3d46152e89
tarik-gadoumi <tarik.gadoumi@laplateforme.io>
2022-09-25 13:02:58 +0200

    *

--- notes/bindActionCreators.js
+++ notes/bindActionCreators.js
@@ -18,13 +18,24 @@ console.log("\u001b[1;43m Section C");
  * * this method allow use to fire dispatches from calling
  * * To understand how bindActionCreators works let's implement it in different ways
  */
-const actionsCreatorOne = compose(store.dispatch, incrementActionCreator);
+const callbackIfExecutedFireDispatchForINCREMENT = compose(
+  store.dispatch,
+  incrementActionCreator
+);
 console.log(
   "firing the dispatch fron calling actionsCreatorOne()",
-  actionsCreatorOne()
+  callbackIfExecutedFireDispatchForINCREMENT()
 );
-console.log("expected 10004", store.getState());
-
+console.log("expected output 10004", store.getState());
+const callbackIfExecutedFireDispatchForADD = compose(
+  store.dispatch,
+  addActionCreator
+);
+console.log(
+  "firing the dispatch fron calling addActionCreator()",
+  callbackIfExecutedFireDispatchForADD(10000)
+);
+console.log("expected output 20004", store.getState());
 /**
  * * what if we had multiple action creators ?
  * *
